0c5f6d5d1d865d6f5e43cae46f6eda6c0d59b7f4
Jason To Sing Hin <sht2@st-andrews.ac.uk>
2022-03-18 17:06:40 +0000

    Update REPL.hs
    -removed subr from process's argument

--- REPL.hs
+++ REPL.hs
@@ -7,12 +7,11 @@ import System.Console.Haskeline
 import Data.List (isPrefixOf)
 import Control.Monad.Trans.State.Strict
 import Control.Monad.Trans.Class
-import Debug.Trace
 
-data LState = LState { vars :: [(Name, Value)] }
+data LState = LState { vars :: [(Name, Value)], forLoop :: Bool }
 
 initLState :: LState
-initLState = LState []
+initLState = LState [] False
 
 -- Given a variable name and a value, return a new set of variables with
 -- that name and value added.
@@ -24,8 +23,8 @@ updateVars name val vars = (name, val) : filter (\var -> fst var /= name) vars
 dropVar :: Name -> [(Name, Value)] -> [(Name, Value)]
 dropVar name = filter (\var -> fst var /= name)
 
-process :: LState -> Command -> Bool -> InputT (StateT LState IO) LState
-process st (Set var e) subr = do
+process :: LState -> Command -> InputT (StateT LState IO) LState
+process st (Set var e) = do
      outputStrLn $ show (eval list e)
      case e of
           Val (StrVal "input") -> do inp <- getInputLine "> "
@@ -42,63 +41,59 @@ process st (Set var e) subr = do
                     Just val -> st {vars = updateVars var val list}
                     Nothing -> st
            exit st = do lift $ put st
-                        if subr then return st else repl st
-process st (Print e) subr
+                        return st
+process st (Print e)
      = do outputStrLn $ show (Print e)
           case eval (vars st) e of
                     Just val -> outputStrLn $ show val
                     Nothing -> outputStrLn "No entry found"
           -- Print the result of evaluation
-          if subr then return st else repl st
-process st (Cond cond x y) subr
+          return st
+process st (Cond cond x y)
      = do outputStrLn $ show (Cond cond x y)
           case eval list cond of
-            Just (NumVal (Int int)) -> do st' <- if int /= 0 then process st x True
-                                                             else process st y True
-                                          if subr then return st' else repl st
+            Just (NumVal (Int int)) -> do if int /= 0 then process st x
+                                                      else process st y
             _ -> do outputStrLn "Non-deterministic condition, action aborted"
-                    if subr then return st else repl st
+                    return st
           where list = vars st
-process st (Repeat acc cmd) subr
+process st (Repeat acc cmd)
      = do outputStrLn $ show (Repeat acc cmd)
           if acc > 0 && not (null cmd) then do st' <- go st cmd
-                                               process st' (Repeat (acc-1) cmd) subr
+                                               process st' (Repeat (acc-1) cmd)
           else do outputStrLn "--Repeat loop exits--" --debug
-                  if subr then return st else repl st
-          where go st (x:xs)  = do st' <- process st x True
+                  return st
+          where go st (x:xs)  = do st' <- process st x
                                    go st' xs
                 go st [] = return st
-process st (While cond cmd) subr
+process st (While cond cmd)
      = do outputStrLn $ show (While cond cmd)
-          st' <- process st (Cond cond (Repeat 1 cmd) (Print $ str "--While loop exits--")) True --debug
-          st'' <- if bool then process st' (While cond cmd) True else return st'
-          if subr then return st'' else repl st''
+          st' <- process st (Cond cond (Repeat 1 cmd) (Print $ str "--While loop exits--")) --debug
+          if bool then process st' (While cond cmd) else return st'
           where bool = case eval list (If cond (int 1) (int 0)) of
                          Just (NumVal (Int 1)) -> True
                          _ -> False
                 list = vars st
                 str val = Val $ StrVal val
                 int val = Val $ NumVal $ Int val
-process st (DoWhile cond cmd) subr
+process st (DoWhile cond cmd)
      = do outputStrLn $ show (DoWhile cond cmd)
-          st' <- process st (Repeat 1 cmd) True -- Do part
-          st'' <- process st' (While cond cmd) True -- While part
-          if subr then return st'' else repl st''
-process st (For init cond after cmd) subr
+          st' <- process st (Repeat 1 cmd) -- Do part
+          process st' (While cond cmd) -- While part
+process st (For init cond after cmd)
      = do outputStrLn $ show (For init cond after cmd)
-          st' <- if subr then return st else go st init
-          st'' <- if bool st' then process st' (Repeat 1 cmd) True else return st'
+          st' <- if forLoop st then return st else do go st init
+          st'' <- if bool st' then process st' (Repeat 1 cmd) else return st'
           st' <- if bool st'' then go st'' after else return st''
-          st'' <- if bool st' then process st' (For init cond after cmd) True else return st'
-          if subr then return st'' else repl st''
-          where go st (x:xs)  = do st' <- process st x True
+          if bool st' then process st' (For init cond after cmd) else return st' {forLoop = False}
+          where go st (x:xs)  = do st' <- process st x
                                    go st' xs
-                go st [] = return st
+                go st [] = return st {forLoop = True}
                 bool st = case eval (vars st) (If cond (int 1) (int 0)) of
                          Just (NumVal (Int 1)) -> True
                          _ -> False
                 int val = Val $ NumVal $ Int val
-process st Quit subr = return st
+process st Quit = return st
 
 commandList = ["input", "print", "if", "then", "else",
                "repeat", "while", "do", "for", "quit"]
@@ -117,6 +112,7 @@ repl :: LState -> InputT (StateT LState IO) LState
 repl st = do inp <- getInputLine "> "
              case parse pCommand $ fromMaybe "" inp of
                     [(cmd, "")] -> -- Must parse entire input
-                                   process st cmd False
+                                   do st' <- process st cmd
+                                      repl st'
                     _ -> do outputStrLn "Parse error"
                             repl st
